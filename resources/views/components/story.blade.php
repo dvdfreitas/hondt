290e7a4bd40e75cb2b9149d3682633271a881661
dvdfreitas <dvdfreitas@gmail.com>
2022-03-14 10:58:01 +0000

    220314 - Migrations and components

--- resources/views/components/story.blade.php
+++ resources/views/components/story.blade.php
@@ -1,4 +1,5 @@
-<div class="border bg-red-400 p-2 text-white">
+@props(['story'])
+<div class="border bg-red-400 p-2 text-white hover:bg-red-500">
     <div class="font-bold">{{ $story->title }}</div>
     <div>{{ $story->subtitle }}</div>
 </div>
